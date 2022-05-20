a8f20d81d9e621e2911facf1938dda5e432a86a7
jstafford <CatMouse2017!>
2022-05-20 15:28:14 -0500

    Checkpoint after finishing video.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using System.Text;
+using static System.Console;
 
 namespace BuilderDesignPattern
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string hello = "hello";
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("<p>");
+            stringBuilder.Append(hello);
+            stringBuilder.Append("</p>");
+            WriteLine(stringBuilder);
+
+            var words = new[] {"hello", "world"};
+            stringBuilder.Clear();
+            stringBuilder.Append("<ul>");
+            foreach (var word in words)
+            {
+                stringBuilder.AppendFormat("<li>{0}</li>", word);
+            }
+            stringBuilder.Append("</ul>");
+            
+            WriteLine(stringBuilder);
         }
     }
 }
